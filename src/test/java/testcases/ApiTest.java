7ea1c992dda67db2c1f0adeef3c83dc178e84e5d
faizul-islam <faizulcse@gmail.com>
2019-10-06 13:44:44 +0600

    add cucumber-junit dependency & update test-runner class

--- src/test/java/testcases/ApiTest.java
+++ src/test/java/testcases/ApiTest.java
@@ -17,7 +17,8 @@ public class ApiTest {
                 .param("id",3)
                 .when().get("/api/users")
                 .then()
-                .extract().response();
+                .extract()
+                .response();
 
         // print response data
         System.out.println(response.statusCode());
@@ -37,5 +38,71 @@ public class ApiTest {
         Assert.assertEquals(jsonPath.get("data.first_name"),"Emma");
         Assert.assertEquals(jsonPath.get("data.last_name"),"Wong");
         Assert.assertEquals(jsonPath.get("data.email"),"emma.wong@reqres.in");
+    }
+
+    @Test
+    public void postMethod(){
+        String payload = " {\n" +
+                "        \"name\": \"Faizul\",\n" +
+                "        \"post\": \"SQA\",\n" +
+                "        \"address\":{\n" +
+                "            \"road\": \"4\",\n" +
+                "            \"house\": \"38/4\",\n" +
+                "            \"city\": \"Dhaka\"\n" +
+                "        }\n" +
+                "    }";
+        RestAssured.baseURI = "https://reqres.in";
+        Response response = given()
+                .body(payload)
+                .when().post("/api/users")
+                .then()
+                .extract()
+                .response();
+
+        System.out.println(response.statusCode());
+        response.prettyPrint();
+
+        JsonPath jsonPath = new JsonPath(response.asString());
+        System.out.println(jsonPath.get("size()"));
+        System.out.println(jsonPath.get("id"));
+    }
+
+    @Test
+    public void all_First_Name(){
+        RestAssured.baseURI = "https://reqres.in";
+        Response response = given()
+                //.log().all()
+                .param("page",1)
+                .when().get("/api/users")
+                .then()
+                .extract()
+                .response();
+
+        System.out.println(response.statusCode());
+        response.prettyPrint();
+
+        JsonPath jsonPath = new JsonPath(response.asString());
+        int size = jsonPath.get("data.size()");
+
+        for(int i = 0; i<size; i++){
+            System.out.println(jsonPath.get("data["+i+"].first_name"));
+        }
+    }
+
+    @Test
+    public void cCheckServer(){
+        RestAssured.baseURI = "https://reqres.in";
+        Response response = given()
+                //.log().all()
+                .param("id",4)
+                .when().get("api/users")
+                .then()
+                .extract()
+                .response();
+
+        System.out.println(response.statusCode());
+        System.out.println(response.header("Server"));
+        System.out.println(response.contentType());
+        response.prettyPrint();
     }
 }
--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -1,5 +1,13 @@
 package testrunner;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "features",  glue = "steps",
+        plugin = {"pretty","html:target/cucumber-html", "json:target/cucumber.json"})
+
 public class TestRunner {
 
 }
